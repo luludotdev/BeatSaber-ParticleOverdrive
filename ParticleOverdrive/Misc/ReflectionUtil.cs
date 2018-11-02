ad17daf760aaaec1c5c46377b6c1ce453f5c4f88
Jack Baron <jackmbaron@gmail.com>
2018-11-02 21:13:03 +0000

    Harmony patch the particles

---
+++ ParticleOverdrive/Misc/ReflectionUtil.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace ParticleOverdrive.Misc
+{
+    public static class ReflectionUtil
+    {
+        [Obsolete]
+        public static void SetPrivateField(this object obj, string fieldName, object value)
+        {
+            obj.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).SetValue(obj, value);
+        }
+
+        [Obsolete]
+        public static T GetPrivateField<T>(this object obj, string fieldName)
+        {
+            return (T)((object)obj.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic).GetValue(obj));
+        }
+
+        [Obsolete]
+        public static void SetPrivateProperty(this object obj, string propertyName, object value)
+        {
+            obj.GetType().GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).SetValue(obj, value, null);
+        }
+
+        [Obsolete]
+        public static void InvokePrivateMethod(this object obj, string methodName, object[] methodParams)
+        {
+            obj.GetType().GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic).Invoke(obj, methodParams);
+        }
+
+        public static void SetField(this object obj, string fieldName, object value)
+        {
+            ((obj is Type) ? ((Type)obj) : obj.GetType()).GetField(fieldName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).SetValue(obj, value);
+        }
+
+        public static object GetField(this object obj, string fieldName)
+        {
+            return ((obj is Type) ? ((Type)obj) : obj.GetType()).GetField(fieldName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).GetValue(obj);
+        }
+
+        public static T GetField<T>(this object obj, string fieldName)
+        {
+            return (T)((object)obj.GetField(fieldName));
+        }
+
+        public static void SetProperty(this object obj, string propertyName, object value)
+        {
+            ((obj is Type) ? ((Type)obj) : obj.GetType()).GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).SetValue(obj, value, null);
+        }
+
+        public static object GetProperty(this object obj, string propertyName)
+        {
+            return ((obj is Type) ? ((Type)obj) : obj.GetType()).GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).GetValue(obj);
+        }
+
+        public static T GetProperty<T>(this object obj, string propertyName)
+        {
+            return (T)((object)obj.GetProperty(propertyName));
+        }
+
+        public static T InvokeMethod<T>(this object obj, string methodName, params object[] methodParams)
+        {
+            return (T)((object)obj.InvokeMethod(methodName, methodParams));
+        }
+
+        public static object InvokeMethod(this object obj, string methodName, params object[] methodParams)
+        {
+            return ((obj is Type) ? ((Type)obj) : obj.GetType()).GetMethod(methodName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).Invoke(obj, methodParams);
+        }
+    }
+}
---
+++ ParticleOverdrive/Patches/NoteCutParticlesEffectSpawnParticles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using UnityEngine;
+using ParticleOverdrive.Misc;
+
+namespace ParticleOverdrive.Patches
+{
+    [HarmonyPatch(typeof(NoteCutParticlesEffect))]
+    [HarmonyPatch("SpawnParticles")]
+    class NoteCutParticlesEffectSpawnParticles
+    {
+        static void Prefix(ref NoteCutParticlesEffect __instance, ref int sparkleParticlesCount, ref int explosionParticlesCount)
+        {
+            float count = 100f;
+
+            ParticleSystem.MainModule main = ((ParticleSystem)__instance.GetField("_sparklesPS")).main;
+
+            sparkleParticlesCount = 150 * Mathf.FloorToInt(count);
+            main.maxParticles = 150 * Mathf.FloorToInt(count * 2f);
+        }
+    }
+}
