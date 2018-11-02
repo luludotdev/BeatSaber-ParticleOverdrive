343a7655d87d35b0fd321dd5a5b3b8052b7b2bae
Jack Baron <jackmbaron@gmail.com>
2018-11-02 20:44:56 +0000

    Setup Plugin and Harmony

---
+++ ParticleOverdrive/Misc/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ParticleOverdrive.Misc
+{
+    static class Logger
+    {
+        public static void Log(object data)
+        {
+            Console.WriteLine($"[Particle Overdrive] {data}");
+        }
+
+        public static void Debug(object data)
+        {
+#if DEBUG
+            Console.WriteLine($"[Particle Overdrive] {data}");
+#endif
+        }
+    }
+}
---
+++ ParticleOverdrive/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using IllusionPlugin;
+using ParticleOverdrive.Misc;
+
+namespace ParticleOverdrive
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "Particle Overdive";
+        public string Version => "0.1.0";
+
+        public void OnApplicationStart()
+        {
+            try
+            {
+                HarmonyInstance harmony = HarmonyInstance.Create("com.jackbaron.beatsaber.particleoverdrive");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception e)
+            {
+                Logger.Log("This plugin requires Harmony. Make sure you " +
+                    "installed the plugin properly, as the Harmony DLL should have been installed with it.");
+                Console.WriteLine(e);
+            }
+        }
+
+        public void OnApplicationQuit() { }
+
+        public void OnLevelWasInitialized(int level) { }
+
+        public void OnLevelWasLoaded(int level) { }
+
+        public void OnUpdate() { }
+
+        public void OnFixedUpdate() { }
+    }
+}
