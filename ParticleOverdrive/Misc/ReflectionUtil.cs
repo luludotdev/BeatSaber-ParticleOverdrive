1f91668ae350343f1e353ef872d884dc5cd850a6
Jack Baron <jackmbaron@gmail.com>
2018-11-26 12:51:56 +0000

    Enhance ReflectionUtil

--- ParticleOverdrive/Misc/ReflectionUtil.cs
+++ ParticleOverdrive/Misc/ReflectionUtil.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ParticleOverdrive.Misc
 {
     public static class ReflectionUtil
     {
+        private const BindingFlags _allBindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+
         [Obsolete]
         public static void SetPrivateField(this object obj, string fieldName, object value)
         {
@@ -32,12 +36,16 @@ namespace ParticleOverdrive.Misc
 
         public static void SetField(this object obj, string fieldName, object value)
         {
-            ((obj is Type) ? ((Type)obj) : obj.GetType()).GetField(fieldName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).SetValue(obj, value);
+            ((obj is Type) ? ((Type)obj) : obj.GetType())
+                .GetField(fieldName, _allBindingFlags)
+                .SetValue(obj, value);
         }
 
         public static object GetField(this object obj, string fieldName)
         {
-            return ((obj is Type) ? ((Type)obj) : obj.GetType()).GetField(fieldName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).GetValue(obj);
+            return ((obj is Type) ? ((Type)obj) : obj.GetType())
+                .GetField(fieldName, _allBindingFlags)
+                .GetValue(obj);
         }
 
         public static T GetField<T>(this object obj, string fieldName)
@@ -47,13 +55,41 @@ namespace ParticleOverdrive.Misc
 
         public static void SetProperty(this object obj, string propertyName, object value)
         {
-            ((obj is Type) ? ((Type)obj) : obj.GetType()).GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).SetValue(obj, value, null);
+            ((obj is Type) ? ((Type)obj) : obj.GetType())
+                .GetProperty(propertyName, _allBindingFlags)
+                .SetValue(obj, value, null);
         }
 
         public static object GetProperty(this object obj, string propertyName)
         {
-            return ((obj is Type) ? ((Type)obj) : obj.GetType()).GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).GetValue(obj);
+            return ((obj is Type) ? ((Type)obj) : obj.GetType())
+                .GetProperty(propertyName, _allBindingFlags)
+                .GetValue(obj);
         }
+
+        public static object InvokeMethod(this object obj, string methodName, object[] methodParams)
+        {
+            return (obj is Type ? (Type)obj : obj.GetType())
+                .GetMethod(methodName, _allBindingFlags)
+                .Invoke(obj, methodParams);
+        }
+
+        public static T InvokeMethod<T>(this object obj, string methodName, object[] methodParams)
+        {
+            return (T)((object)obj.InvokeMethod(methodName, methodParams));
+        }
+
+        public static object InvokeConstructor(this object obj, params object[] constructorParams)
+        {
+            Type[] types = new Type[constructorParams.Length];
+            for (int i = 0; i < constructorParams.Length; i++) types[i] = constructorParams[i].GetType();
+
+            return (obj is Type ? (Type)obj : obj.GetType())
+                .GetConstructor(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null, types, null)
+                .Invoke(constructorParams);
+        }
+
+        public static Type GetStaticType(string @class) => Type.GetType(@class);
 
         public static Component CopyComponent(Component original, Type originalType, Type overridingType, GameObject destination)
         {
@@ -77,6 +113,19 @@ namespace ParticleOverdrive.Misc
             {
                 fi.SetValue(destination, fi.GetValue(source));
             }
+        }
+
+        public static IEnumerable<Assembly> ListLoadedAssemblies() => AppDomain.CurrentDomain.GetAssemblies();
+
+        public static IEnumerable<string> ListNamespacesInAssembly(Assembly assembly)
+        {
+            IEnumerable<string> ret = Enumerable.Empty<string>();
+            ret = ret.Concat(assembly.GetTypes()
+                .Select(t => t.Namespace)
+                .Distinct()
+                .Where(n => n != null));
+
+            return ret.Distinct();
         }
     }
 }
